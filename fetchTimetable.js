c20c3700937ae09ac48e79f27e3a02beb46b3770
Johnson Chin <johnsonchin1009@gmail.com>
2025-02-17 12:14:00 +0800

    feat: added grouping filter in fetchTimetable.js

--- fetchTimetable.js
+++ fetchTimetable.js
@@ -3,16 +3,17 @@ config();
 
 const s3Endpoint = process.env.APU_TIMETABLE_S3;
 const intakeCode = process.env.INTAKE_CODE;
-const nonElectives = [""]; // Add MODID that is NOT your elective
+const nonElectives = ["CT027-3-3-EPDA", "CT024-3-3-DCOMS"]; // Add MODID that is NOT your elective
+const grouping = "G1"; // G1 is default, grouping to filter by
 
 export async function fetchTimetable() {
   if (!s3Endpoint) {
-    console.error("Error: S3 endpoint not configured");
+    console.error("❌ Error: S3 endpoint not configured");
     return []; // Return empty array instead of Response
   }
 
   if (!intakeCode) {
-    console.error("Error: Intake Code not configured");
+    console.error("❌ Error: Intake Code not configured");
     return [];
   }
 
@@ -20,7 +21,7 @@ export async function fetchTimetable() {
     const response = await fetch(s3Endpoint);
 
     if (!response.ok) {
-      console.error("Error: Unable to fetch data from S3");
+      console.error("❌ Error: Unable to fetch data from S3");
       return [];
     }
 
@@ -29,13 +30,15 @@ export async function fetchTimetable() {
     // Filter the entire S3 dataset by intake code
     const filteredData = filterByIntake(data, intakeCode);
 
-    // Filter the courses by the user's elective choices
-    const result = filterByElective(filteredData, nonElectives);
+    const filteredByGrouping = filterByGrouping(filteredData, grouping);
 
-    return result; // Return data directly as an array
+    // Filter the courses by the user's elective choices
+    const result = filterByElective(filteredByGrouping, nonElectives);
+
+    return result; // ✅ Return data directly as an array
   } catch (error) {
-    console.error("Error fetching data:", error.message);
-    return []; // Return empty array on error
+    console.error("❌ Error fetching data:", error.message);
+    return []; // ✅ Return empty array on error
   }
 }
 
@@ -56,5 +59,13 @@ const filterByElective = (filteredData, nonElectives) => {
     const modid = entry.MODID?.trim();
     const isNonElective = nonElectives.some((mod) => modid?.includes(mod));
     return !isNonElective; // Keep only those that are NOT nonElectives
+  });
+};
+
+const filterByGrouping = (filteredData, grouping) => {
+  return filteredData.filter((entry) => {
+    const groupingValue = entry.GROUPING?.trim();
+    const searchValue = grouping.trim();
+    return groupingValue === searchValue;
   });
 };
